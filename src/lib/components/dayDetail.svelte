52bb1ef854faa54d33f52786d7fc6eabbfedc0dd
argom01 <nor.siczek@tutanota.com>
2022-11-07 11:49:33 +0100

    coconut oily smooth animations

--- src/lib/components/dayDetail.svelte
+++ src/lib/components/dayDetail.svelte
@@ -17,20 +17,14 @@
     let minH =
         100 - ($headerHeight / window.innerHeight) * 100 - ($calendarGridHeight / window.innerHeight) * 100;
 
-    let expandingDivHeight = 0;
     let div: HTMLElement;
     let scroll: any;
-
-    // $: if (div && expandingDivHeight !== 0) {
-    //     div.style.height = `${expandingDivHeight}px`;
-    // }
 
     async function scrollDown() {
         scroll = setInterval(() => {
             if (document.body.scrollHeight / window.innerHeight < 1.5) {
                 window.scrollTo({
                     top: document.body.scrollHeight,
-                    left: 0,
                     behavior: "smooth",
                 });
             }
@@ -40,11 +34,9 @@
     let currentHeight: number;
     let newHeight: number;
 
-        let didIntroCancel = false;
     function onUse(e: HTMLElement, _l: number) {
         div = e;
 
-        const expandingDiv = e;
         const dayDetail = document.getElementById("day-detail-list")!.firstElementChild;
         const addButton = document.getElementById("add-event-button");
         if (!dayDetail) {
@@ -54,61 +46,40 @@
         dayDetailListElementHeight.set(dayDetail!.clientHeight);
         addButtonHeight.set(addButton!.clientHeight);
 
-        // div.style.height = `${e.clientHeight}px`;
-        div.addEventListener("transitionend", () => {
-            clearInterval(scroll);
-            if (document.body.scrollHeight / window.innerHeight < 1.5) {
-                window.scrollTo({
-                    top: document.body.scrollHeight,
-                    behavior: "smooth",
-                });
-            }
-        });
-        div.addEventListener("transitioncancel", () => {
-            console.log("dupa");
-        });
-        div.addEventListener("animationcancel", () => {
-            console.log("cipsko")
-            if (document.body.scrollHeight / window.innerHeight < 1.5) {
-                window.scrollTo({
-                    top: document.body.scrollHeight,
-                    behavior: "smooth",
-                });
-            }
-            // window.scrollBy({
-            //     top: 10,
-            //     behavior: "smooth",
-            // });
-        });
-        div.addEventListener("animationend", () => {
-            console.log("chuj");
-        });
-        div.addEventListener("transitionstart", () => scrollDown());
-        currentHeight = e.clientHeight;
-
         beforeUpdate(() => {
             if (div) {
                 document.body.style.height = document.body.clientHeight + "px";
             }
         });
 
+        currentHeight = e.clientHeight;
+
         let transition: any;
         return {
-            update(l: number) {
-                const calculatedHeight = $addButtonHeight + l * $dayDetailListElementHeight;
-                if ((calculatedHeight / window.innerHeight) * 100 > minH) {
-                    expandingDivHeight = calculatedHeight;
-                } else {
-                    expandingDivHeight = (minH * window.innerHeight) / 100;
-                }
-
+            update(_l: number) {
                 if (e.style.animationPlayState === "running") {
                     document.body.style.height = "";
-                    return 
+                    e.style.animationName = "";
+                    e.style.animationPlayState = "";
+                    e.style.animationDuration = "";
+                    e.style.animationFillMode = "";
+                    e.style.animationTimingFunction = "";
+                    e.style.animationDirection = "";
+                    e.style.animationDelay = "";
+                    e.style.animationIterationCount = "";
+                    document.body.style.height = "";
+                    newHeight = e.clientHeight;
+                    currentHeight = newHeight;
+                    if (document.body.scrollHeight / window.innerHeight < 1.5) {
+                        window.scrollTo({
+                            top: document.body.scrollHeight,
+                            behavior: "smooth",
+                        });
+                    }
+                    return;
                 }
 
-                console.log(transition);
-                if (transition ) {
+                if (transition) {
                     transition.cancel();
                     document.body.style.height = "";
                     newHeight = e.clientHeight;
@@ -122,7 +93,6 @@
                     return;
                 }
                 newHeight = e.clientHeight;
-                console.log(currentHeight, newHeight);
                 scrollDown();
                 transition = e.animate(
                     [
@@ -135,7 +105,7 @@
                             overflow: "hidden",
                         },
                     ],
-                    { duration: 3000 }
+                    { duration: 1000, easing: "ease-out" }
                 );
                 document.body.style.height = "";
                 transition.onfinish = () => {
@@ -165,7 +135,7 @@
     on:outrostart={() => {
         document.body.style.height = "";
     }}
-    class="flex justify-center overflow-hidden  bg-neutral-900 "
+    class="flex justify-center overflow-hidden bg-neutral-900"
     id="expanding-div"
 >
     <div class="w-10/12" style={`min-height: ${minH}vh;`} id="day-detail-list-wrapper">
